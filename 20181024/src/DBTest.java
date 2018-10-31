9db4015803e2ba84632becd6f00d0ff443f46cb8
Akira Shinohara <k017c1067@it-neec.jp>
2018-10-31 13:01:08 +0900

    [new] 2018.10.24

---
+++ 20181024/src/DBTest.java
@@ -0,0 +1,29 @@
+import java.sql.*;
+
+/**
+ * @author Akira Shinohara
+ * @since 2018.10.24
+ */
+public class DBTest {
+    public static void main(String[] args) {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/free_kukan", "free_kukan", "free");
+            Statement stat = conn.createStatement();
+            ResultSet result = stat.executeQuery("SELECT * FROM users");
+            while(result.next()) {
+                String user_id = result.getString("user_id");
+                String first_name = result.getString("name");
+                String last_name = result.getString("birth_of_day");
+                String post_code = result.getString("post_code");
+                String address = result.getString("address");
+                String created_at = result.getString("created_at");
+                String updated_at = result.getString("updated_at");
+                System.out.println(user_id + "\t" + first_name + "\t" + last_name + "\t" + post_code + "\t" + address + "\t" + created_at + "\t" + updated_at);
+            }
+            conn.close();
+        } catch (Exception err){
+            System.err.println(err.toString());
+        }
+    }
+}
